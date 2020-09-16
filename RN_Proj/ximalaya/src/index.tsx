546ce3d44715d23856fd01deaa66ef0ac2505753
Xiaohai.lin <tbfungeek@163.com>
2020-09-16 23:08:29 +0800

    1. 导入react-native-config 2.组织项目目录

---
+++ RN_Proj/ximalaya/src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+} from 'react-native';
+
+import Config from "react-native-config";
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+          <Text>{Config.API_URL}</Text>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignContent:'center',
+  }
+});
+
+export default App;
