8c87780b5d543828733daacbd4e2432af922c60c
Xiaohai.lin <tbfungeek@163.com>
2020-09-22 22:52:55 +0800

    添加statusbar

--- RN_Proj/ximalaya/src/index.tsx
+++ RN_Proj/ximalaya/src/index.tsx
@@ -1,5 +1,6 @@
 import Navigator from '@/navigators/StackNavigator';
 
+import {StatusBar} from 'react-native';
 import React from 'react';
 import {Provider} from 'react-redux';
 import store from '@/model/dva';
@@ -8,6 +9,11 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
+        <StatusBar
+          backgroundColor={'transparent'}
+          barStyle={'dark-content'}
+          translucent
+        />
         <Navigator />
       </Provider>
     );
