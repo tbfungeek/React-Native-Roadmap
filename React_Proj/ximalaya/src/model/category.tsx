656820f186d7a576dc014493d0e7bc211c3f76e8
Xiaohai.lin <tbfungeek@163.com>
2020-10-02 15:31:59 +0800

    rootstate

---
+++ React_Proj/ximalaya/src/model/category.tsx
@@ -0,0 +1,92 @@
+import {Model, Effect, SubscriptionsMapObject} from 'dva-core-ts';
+import {Reducer} from 'redux';
+import {load} from './storage/storage';
+import storage from './storage/storage';
+import axios from 'axios';
+
+const CATEGORY_URL = '/mock/11/ximalaya/category';
+
+interface ICategory {
+  id: string;
+  name: string;
+  classify?: string;
+}
+
+interface CategoryModelState {
+  myCategories: ICategory[];
+  categories: ICategory[];
+}
+
+interface CategoryModel extends Model {
+  namespace: 'category';
+  state: CategoryModelState;
+  effects: {
+    loadData: Effect;
+  };
+  reducers: {
+    setState: Reducer<CategoryModelState>;
+  };
+  subscriptions: SubscriptionsMapObject;
+}
+
+const initialState = {
+  myCategories: [
+    {id: 'home', name: '推荐'},
+    {id: 'vip', name: 'Vip'},
+  ],
+  categories: [],
+};
+
+const categoryModel: CategoryModel = {
+  namespace: 'category',
+  state: initialState,
+  effects: {
+    *loadData(_, {call, put}) {
+      // 从storage获取数据
+      const myCategories = yield call(load, {key: 'myCategories'});
+      const categories = yield call(load, {key: 'categories'});
+      //发送数据存储到state中
+      if (myCategories) {
+        yield put({
+          type: 'setState',
+          payload: {
+            myCategories,
+            categories,
+          },
+        });
+      } else {
+        yield put({
+          type: 'setState',
+          payload: {
+            categories,
+          },
+        });
+      }
+    },
+  },
+  reducers: {
+    setState(state, {payload}) {
+      return {
+        ...state,
+        ...payload,
+      };
+    },
+  },
+  subscriptions: {
+    setup({dispatch}) {
+      dispatch({type: 'loadData'});
+    },
+    asyncStorage() {
+      storage.sync.categorys = async () => {
+        const {data} = await axios.get(CATEGORY_URL);
+        return data;
+      };
+
+      storage.sync.myCategories = async () => {
+        return null;
+      };
+    },
+  },
+};
+
+export default categoryModel;
--- React_Proj/ximalaya/src/model/index.tsx
+++ React_Proj/ximalaya/src/model/index.tsx
@@ -1,10 +1,12 @@
 import home from './home';
 import {DvaLoadingState} from 'dva-loading-ts';
+import category from './category';
 
 const models = [home];
 
 export type RootState = {
   home: typeof home.state;
+  category: typeof category.state;
   loading: DvaLoadingState;
 };
 
