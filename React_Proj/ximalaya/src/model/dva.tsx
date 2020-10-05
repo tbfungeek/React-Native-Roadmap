757a6b1d523d6a59890dc49a5256284b5f7a6681
Xiaohai.lin <tbfungeek@163.com>
2020-10-05 15:14:41 +0800

    使用model-extends 动态生成model

--- React_Proj/ximalaya/src/model/dva.tsx
+++ React_Proj/ximalaya/src/model/dva.tsx
@@ -1,7 +1,9 @@
-import {create} from 'dva-core-ts';
+import {create, Model} from 'dva-core-ts';
 import modules from '@/model/index';
 import createLoading from 'dva-loading-ts';
 import createLogger from 'dva-logger';
+import modelExtend from 'dva-model-extend';
+import homeModel from './home';
 
 //创建应用
 const app = create();
@@ -19,3 +21,22 @@ app.start();
 
 //导出数据
 export default app._store;
+
+interface Cached {
+  [key: string]: boolean;
+}
+const cached: Cached = {
+  home: true,
+};
+
+function registerModel(model: Model) {
+  if (!cached[model.namespace]) {
+    app.model(model);
+    cached[model.namespace] = true;
+  }
+}
+
+export function createHomeModel(namespace: string) {
+  const model = modelExtend(homeModel, {namespace});
+  registerModel(model);
+}
