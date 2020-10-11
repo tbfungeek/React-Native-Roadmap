257ea34d73732212bbaa3ef1999aaa4f2dcfd844
Xiaohai.lin <tbfungeek@163.com>
2020-10-11 20:24:54 +0800

    调整模态导航器

--- React_Proj/ximalaya/src/navigators/StackNavigator.tsx
+++ React_Proj/ximalaya/src/navigators/StackNavigator.tsx
@@ -72,42 +72,72 @@ const styles = StyleSheet.create({
   },
 });
 
+function RootStackScreens() {
+  return (
+    <Stack.Navigator
+      headerMode="float"
+      screenOptions={{
+        headerTitle: '首页',
+        headerTitleAlign: 'center',
+        headerStyleInterpolator: HeaderStyleInterpolators.forUIKit,
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        gestureEnabled: true,
+        gestureDirection: 'horizontal',
+        headerStatusBarHeight: StatusBar.currentHeight,
+        headerTitleStyle: {
+          color: 'white',
+          fontSize: 16,
+        },
+        headerStyle: {
+          backgroundColor: '#e94922',
+          ...Platform.select({
+            android: {
+              borderBottomWidth: StyleSheet.hairlineWidth,
+            },
+          }),
+        },
+      }}>
+      <Stack.Screen name="TabNavigator" component={TabNavigator} />
+      <Stack.Screen name="Detail" component={Detail} />
+      <Stack.Screen name="Category" component={Category} />
+      <Stack.Screen name="Album" component={Album} options={getAlbumOptions} />
+    </Stack.Navigator>
+  );
+}
+
+/**
+ * 定义每个界面传递参数的类型
+ */
+export type ModelStackParamList = {
+  Root: undefined /** 跳转到Root页面传递的参数 */;
+  Detail: undefined /** 跳转到Detail页面传递的参数 */;
+};
+
+/**
+ * 创建出对应的导航器
+ */
+const ModelStack = createStackNavigator<ModelStackParamList>();
+
+function ModelStackScreens() {
+  return (
+    <ModelStack.Navigator mode="modal" headerMode="screen">
+      <ModelStack.Screen
+        name="Root"
+        component={RootStackScreens}
+        options={{headerShown: false}}
+      />
+      <ModelStack.Screen name="Detail" component={Detail} />
+    </ModelStack.Navigator>
+  );
+}
+
+export type ModelStackNavigation = StackNavigationProp<ModelStackParamList>;
+
 export default class StackNavigator extends React.Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator
-          headerMode="float"
-          screenOptions={{
-            headerTitle: '首页',
-            headerTitleAlign: 'center',
-            headerStyleInterpolator: HeaderStyleInterpolators.forUIKit,
-            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-            gestureEnabled: true,
-            gestureDirection: 'horizontal',
-            headerStatusBarHeight: StatusBar.currentHeight,
-            headerTitleStyle: {
-              color: 'white',
-              fontSize: 16,
-            },
-            headerStyle: {
-              backgroundColor: '#e94922',
-              ...Platform.select({
-                android: {
-                  borderBottomWidth: StyleSheet.hairlineWidth,
-                },
-              }),
-            },
-          }}>
-          <Stack.Screen name="TabNavigator" component={TabNavigator} />
-          <Stack.Screen name="Detail" component={Detail} />
-          <Stack.Screen name="Category" component={Category} />
-          <Stack.Screen
-            name="Album"
-            component={Album}
-            options={getAlbumOptions}
-          />
-        </Stack.Navigator>
+        <ModelStackScreens />
       </NavigationContainer>
     );
   }
