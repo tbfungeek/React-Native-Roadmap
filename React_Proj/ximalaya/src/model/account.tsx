bf25d6e43ce179a88f7a4490aaee7897e5bee4be
Xiaohai.lin <tbfungeek@163.com>
2020-10-22 00:11:10 +0800

    将账号密码保存到本地

--- React_Proj/ximalaya/src/model/account.tsx
+++ React_Proj/ximalaya/src/model/account.tsx
@@ -1,8 +1,9 @@
-import {Model, Effect} from 'dva-core-ts';
+import {Model, Effect, SubscriptionsMapObject} from 'dva-core-ts';
 import {Reducer} from 'redux';
 import axios from 'axios';
 import {Alert} from 'react-native';
 import {goBack} from '@/utils/utils';
+import storage, {load} from './storage/storage';
 
 const LOGIN_URL = '/mock/11/ximalaya/login';
 
@@ -21,10 +22,12 @@ interface IAccountModel extends Model {
   effects: {
     login: Effect;
     logOut: Effect;
+    loadStorage: Effect;
   };
   reducers: {
     setState: Reducer<IUserState>;
   };
+  subscriptions: SubscriptionsMapObject;
 }
 
 const initialState: IUserState = {
@@ -42,16 +45,32 @@ const accountModel: IAccountModel = {
           type: 'setState',
           payload: {user: data},
         });
+        storage.save({key: 'user', data: data});
         goBack();
       } else {
         //Alert.alert('登陆失败');
       }
     },
-    *logOut({payload}, {call, put}) {
+    *logOut(_, {put}) {
       yield put({
         type: 'setState',
         payload: {user: null},
       });
+      storage.save({key: 'user', data: null});
+    },
+    *loadStorage(_, {put, call}) {
+      try {
+        const user = yield call(load, {key: 'user'});
+        yield put({
+          type: 'setState',
+          payload: {user: user},
+        });
+      } catch (error) {
+        yield put({
+          type: 'setState',
+          payload: {user: null},
+        });
+      }
     },
   },
   reducers: {
@@ -60,6 +79,13 @@ const accountModel: IAccountModel = {
         ...state,
         ...payload,
       };
+    },
+  },
+  subscriptions: {
+    setup({dispatch}) {
+      dispatch({
+        type: 'loadStorage',
+      });
     },
   },
 };
