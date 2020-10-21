ed5a66761844c1533a63fcc172926a8fc9ceab5b
Xiaohai.lin <tbfungeek@163.com>
2020-10-22 01:41:37 +0800

    添加按钮节流处理

--- React_Proj/ximalaya/src/components/Common/Touchable.tsx
+++ React_Proj/ximalaya/src/components/Common/Touchable.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {
   TouchableOpacity,
   TouchableOpacityProps,
   StyleSheet,
 } from 'react-native';
+import _ from 'lodash';
 
 const Touchable: React.FC<TouchableOpacityProps> = React.memo(
-  ({style, ...rest}) => {
+  ({style, onPress, ...rest}) => {
     const isDisable = rest.disabled;
     const touchableStyle = isDisable ? [style, styles.disabled] : style;
+    let throttleOnPress;
+    if (typeof onPress === 'function') {
+      throttleOnPress = useCallback(
+        _.throttle(onPress, 1000, {leading: true, trailing: false}),
+        [onPress],
+      );
+    }
     return (
-      <TouchableOpacity {...rest} style={touchableStyle} activeOpacity={0.8} />
+      <TouchableOpacity
+        {...rest}
+        onPress={throttleOnPress}
+        style={touchableStyle}
+        activeOpacity={0.8}
+      />
     );
   },
 );
