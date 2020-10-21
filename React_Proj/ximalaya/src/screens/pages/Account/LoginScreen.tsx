3f42f481a6f3b8bfbbca73cde339c87536a6accf
Xiaohai.lin <tbfungeek@163.com>
2020-10-21 21:17:46 +0800

    引入yup表单校验

--- React_Proj/ximalaya/src/screens/pages/Account/LoginScreen.tsx
+++ React_Proj/ximalaya/src/screens/pages/Account/LoginScreen.tsx
@@ -5,6 +5,7 @@ import {TextInput} from 'react-native-gesture-handler';
 import Touchable from '@/components/Common/Touchable';
 import {RootState} from '@/model/index';
 import {ConnectedProps, connect} from 'react-redux';
+import * as Yup from 'yup';
 
 interface Values {
   account: string;
@@ -26,6 +27,11 @@ const connector = connect(mapStateToProps);
 
 type ModelState = ConnectedProps<typeof connector>;
 
+const validationSchema = Yup.object().shape({
+  account: Yup.string().trim().required('请输入您的账号'),
+  password: Yup.string().trim().required('请输入密码'),
+});
+
 interface IProps extends ModelState {}
 
 class LoginScreen extends React.Component<IProps> {
@@ -40,8 +46,11 @@ class LoginScreen extends React.Component<IProps> {
     return (
       <ScrollView keyboardShouldPersistTaps="handled">
         <Text style={styles.logo}>听书</Text>
-        <Formik initialValues={initialValues} onSubmit={this.onSubmit}>
-          {({values, handleChange, handleBlur, handleSubmit}) => {
+        <Formik
+          validationSchema={validationSchema}
+          initialValues={initialValues}
+          onSubmit={this.onSubmit}>
+          {({values, handleChange, handleBlur, handleSubmit, errors}) => {
             return (
               <View>
                 <TextInput
@@ -49,13 +58,18 @@ class LoginScreen extends React.Component<IProps> {
                   onBlur={handleBlur('account')}
                   value={values.account}
                 />
+                {errors.account && (
+                  <Text style={styles.errorType}>{errors.account}</Text>
+                )}
                 <TextInput
                   onChangeText={handleChange('password')}
                   onBlur={handleBlur('password')}
                   value={values.password}
                   secureTextEntry
                 />
-
+                {errors.password && (
+                  <Text style={styles.errorType}>{errors.password}</Text>
+                )}
                 <Touchable onPress={handleSubmit}>
                   <Text>登陆</Text>
                 </Touchable>
@@ -69,6 +83,10 @@ class LoginScreen extends React.Component<IProps> {
 }
 
 const styles = StyleSheet.create({
+  errorType: {
+    color: '#ff4000',
+    marginTop: 10,
+  },
   logo: {
     color: '#ff4000',
     fontWeight: 'bold',
