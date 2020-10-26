539cd06914aadf492d06ccec16ed4efe03b7ccb0
linxiaohai <tbfungeek@163.com>
2020-10-26 10:54:02 +0800

    移除无用类

--- React_Proj/admin-frontend/src/admin.js
+++ React_Proj/admin-frontend/src/admin.js
@@ -3,19 +3,19 @@ import {Col, Row} from 'antd';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import NavLeft from './components/NavLeft';
-import './common.less'
+import './Admin.less'
 
 export default class Admin extends React.Component {
 	render() {
 		return (
-            <Row className={container}>
-                <Col span={4} className={navLeftContainer}>
-                    <NavLeft className={navLeft}/>
+            <Row className="Container">
+                <Col span={4} className="NavLeft-container">
+                    <NavLeft className="NavLeft"/>
                 </Col>
-                <Col span={20} className={contentContainer}>
-                    <Header className={header}/>
-                    <Row className={content}>Content</Row>
-                    <Footer className={footer}/>
+                <Col span={20} className="Content-container">
+                    <Header className="header"/>
+                    <Row className="Content">Content</Row>
+                    <Footer className="footer"/>
                 </Col>
             </Row>
         );
