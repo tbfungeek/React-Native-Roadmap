8a068a7c7dab81a02e239309f97d972c31453add
Xiaohai.lin <tbfungeek@163.com>
2020-10-27 01:31:10 +0800

    初步实现侧边菜单栏
    初步实现侧边菜单栏

--- React_Proj/admin-frontend/src/components/NavLeft/index.js
+++ React_Proj/admin-frontend/src/components/NavLeft/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Menu } from 'antd';
+import './index.less';
+import MenuConfig from '../../configs/menuConfigs';
+
+const { SubMenu } = Menu;
 
 export default class NavLeft extends React.Component {
+	renderMenu = (data) => {
+		return data.map((item) => {
+			if (item.children) {
+				return (
+					<SubMenu key={item.key} title={item.title}>
+						{this.renderMenu(item.children)}
+					</SubMenu>
+				);
+			}
+			return <Menu.Item key={item.key} title={item.title}>{item.title}</Menu.Item>;
+		});
+	};
+
+	componentWillMount() {
+        const menuTreeNode = this.renderMenu(MenuConfig);
+        this.setState({
+            menuTreeNode
+        })
+	}
 	render() {
-		return <div>NavLeft</div>;
+		return (
+			<div>
+				<div className="logo">
+					<img src="/assets/logo-ant.svg" alt="" />
+					<h1>CMS</h1>
+				</div>
+                <Menu mode="vertical" theme="dark">
+                    {this.state.menuTreeNode}
+                </Menu>
+			</div>
+		);
 	}
 }
